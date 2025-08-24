95c5c3a4a951fccabf99e684627e693bd316eae7
vipin singh <vipin@pharamaforceiq.com>
2025-08-24 22:05:14 +0530

    addedssd

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -21,7 +21,7 @@ const Command = React.forwardRef<
 ))
 Command.displayName = CommandPrimitive.displayName
 
-interface CommandDialogProps extends DialogProps {} //this is a command dilaouge prop
+interface CommandDialogProps extends DialogProps {} //this is a command dilaouge prop added
 
 const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
   return (
